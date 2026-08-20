fe67328f5e3019af179fc3941b5118e637015293
pavanmsp2786-ctrl <pavanmsp2786@gmail.com>
2026-08-20 10:15:48 +0530

    Added InsertionSort

---
+++ Practical-1/InsertionSort.py
@@ -0,0 +1,17 @@
+def insertion_sort(arr):
+    n = len(arr)
+
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+arr = [10,9.18,20,7,1]
+print("before sorting:",arr)
+
+insertion_sort(arr)
+print("after sorting:",arr)
