fc17ef53535c4cbf72e3fbd572220fc3b5afb99b
pavanmsp2786-ctrl <pavanmsp2786@gmail.com>
2026-08-20 10:19:50 +0530

    Added SelectionSort

---
+++ Practical-1/SelectionSort.py
@@ -0,0 +1,16 @@
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+arr = arr = [10,9.18,20,7,1]
+print("before sorting:",arr)
+
+selection_sort(arr)
+print("after sorting:",arr)
